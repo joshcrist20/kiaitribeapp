dabce3a30287e720d4ad00c6bbb839b22fe54a7d
Josh Crist <joshcrist20@gmail.com>
2016-05-10 10:32:53 -0600

    added images to model and pillow

--- core/models.py
+++ core/models.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.urlresolvers import reverse
 
+import os
+import uuid
+
+def upload_to_location(instance, filename):
+    blocks = filename.split('.')
+    ext = blocks[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    instance.title = blocks[0]
+    return os.path.join('uploads/', filename)
+
 # Create your models here.
 
 class Location(models.Model):
@@ -11,6 +21,7 @@ class Location(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
 	address = models.TextField(null=True, blank=True)
 	hours = models.TextField(null=True, blank=True)
+	image_file = models.ImageField(upload_to=upload_to_location, null=True, blank=True)
 
 	def __unicode__(self):
          return self.title
