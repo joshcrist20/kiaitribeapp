480b2faac4884fbe6d17962498e7173824fea7b6
Josh Crist <joshcrist20@gmail.com>
2016-05-10 09:56:58 -0600

    Added address and hours

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,6 +9,11 @@ class Location(models.Model):
 	title = models.CharField(max_length=300)
 	description = models.TextField(null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
+	address = models.TextField(null=True, blank=True)
+	hours = models.TextField(null=True, blank=True)
 
 	def __unicode__(self):
          return self.title
+
+   	def get_absolute_url(self):
+    	 return reverse(viewname="location_list", args=[self.id])
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 import core.models as coremodels
 
 # Create your views here.
@@ -10,4 +11,9 @@ class LandingView(TemplateView):
 class LocationListView(ListView):
     model = coremodels.Location
     template_name = 'location/list.html'
+
+class LocationDetailView(DetailView):
+    model = coremodels.Location
+    template_name = 'location/detail.html'
+    context_object_name ='location'
 
